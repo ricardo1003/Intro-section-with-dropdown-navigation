de58a3a690b46cec7e5700c59eabb60bf6f67967
unknown <ricardodonado1105@gmail.com>
2024-07-30 18:34:05 -0500

    interactive elements declared as consts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
-console.log("hello world!")
+const navbarElement = document.getElementsByClassName("navbar")[0]
+const openNavButtonElement = document.getElementsByClassName("openNav")[0]
+const expandableElements = document.getElementsByClassName("expand")
