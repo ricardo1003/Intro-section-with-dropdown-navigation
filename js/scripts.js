2ba64d63c24c8e2084b4bc0d8fb8ebf80c39a535
unknown <ricardodonado1105@gmail.com>
2024-07-30 21:15:19 -0500

    added animations for the menu(mobile)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 const navbarElement = document.getElementsByClassName("navbar")[0]
 const openNavButtonElement = document.getElementsByClassName("openNav")[0]
 const expandableElements = document.getElementsByClassName("expand")
+
+
+
+openNavButtonElement.addEventListener("click", ()=>{
+    if(navbarElement.classList.contains("opened")){
+        navbarElement.classList.add("closed")
+    }else{
+        navbarElement.classList.remove("closed")
+    }
+    navbarElement.classList.toggle("opened")
+})
+for (let i = 0; i < expandableElements.length; i++){
+    expandableElements[i].addEventListener("click", ()=>{
+        expandableElements[i].classList.toggle("expanded")
+    }
+)}
